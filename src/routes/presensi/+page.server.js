c6e7714372a9753e3ea56baaf3f50d435d97b769
alannuarii <alannuariii@gmail.com>
2023-02-07 06:49:04 +0800

    Update

--- src/routes/presensi/+page.server.js
+++ src/routes/presensi/+page.server.js
@@ -1,7 +1,7 @@
 import { tanggal } from "../../lib/js/datetime";
 
 export const load = async () => {
-	const res = await fetch(`https://pltdktm.anpy.my.id/${tanggal()}`);
+	const res = await fetch(`https://pltdktm.anpy.my.id/presensi/${tanggal()}`);
 	const data = res.json();
 
 	return data;
